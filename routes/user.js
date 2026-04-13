65fd8b21bdf3320de59ba20bcaf2aad24297fa0a
AADRIKA <aadrika1005@gmail.com>
2026-04-14 01:18:57 +0530

    Add Project Files

---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const router = express.Router();
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middlewares");
+const userController = require("../controllers/user.js");
+
+router
+  .route("/signup")
+  .get(userController.renderSignUpForm)
+  .post(wrapAsync(userController.signup));
+
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login,
+  );
+
+
+
+router.get("/logout", userController.logout);
+
+module.exports = router;
